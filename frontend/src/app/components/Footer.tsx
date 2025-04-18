7e5b3cb1483c0a1753bceccab0e295e86a2e52d0
Matthew Dylewski <94753251+mrdski@users.noreply.github.com>
2025-04-18 16:48:57 -0400

    Update Footer.tsx

--- frontend/src/app/components/Footer.tsx
+++ frontend/src/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer: React.FC = () => {
     return (
         <footer className="mt-12 text-center text-secondary">
-            <p>Powered by Rafael, Andres, Deep, Chandler, and Mathew</p>
+            <p>Powered by Rafael, Andres, Deep, Chandler, and Matthew</p>
             <p className="mt-2 text-sm">
                 © {new Date().getFullYear()} SwampLocks. All rights reserved.
             </p>
